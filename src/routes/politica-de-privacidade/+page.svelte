9d612b55d81b13b4ddcc175b89acac5413d366ee
rodrigohpalmeirim <rod@rodlabs.dev>
2025-06-02 16:05:15 +0100

    add privacy policy

---
+++ src/routes/politica-de-privacidade/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	import Footer from '$lib/components/Footer.svelte';
+	import { Shield } from 'lucide-svelte';
+</script>
+
+<svelte:head>
+	<title>Gira+ | Política de Privacidade</title>
+</svelte:head>
+
+<div class="flex flex-col min-h-screen">
+	<div class="container mx-auto py-12 px-4 max-w-6xl grow">
+		<header class="mb-10">
+			<div class="flex items-center mb-4">
+				<Shield class="min-h-10 min-w-10 text-primary" />
+				<h1 class="text-3xl font-bold ml-2">Política de Privacidade da Gira+</h1>
+			</div>
+		</header>
+
+		<div class="mt-8 mb-4">
+			Os dados são processados de acordo com o código presente em <a class="underline" href="https://github.com/rt-evil-inc/gira-mais-web">https://github.com/rt-evil-inc/gira-mais-web</a>. Os dados não serão associados à sua identidade e serão usados para gerar gráficos públicos e relatar erros aos desenvolvedores.
+		</div>
+	</div>
+
+	<Footer class="bg-muted/40" />
+</div>
---
+++ src/routes/privacy-policy/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	import Footer from '$lib/components/Footer.svelte';
+	import { Shield } from 'lucide-svelte';
+</script>
+
+<svelte:head>
+	<title>Gira+ | Privacy Policy</title>
+</svelte:head>
+
+<div class="flex flex-col min-h-screen">
+	<div class="container mx-auto py-12 px-4 max-w-6xl grow">
+		<header class="mb-10">
+			<div class="flex items-center mb-4">
+				<Shield class="min-h-10 min-w-10 text-primary" />
+				<h1 class="text-3xl font-bold ml-2">Gira+ Privacy Policy</h1>
+			</div>
+		</header>
+
+		<div class="mt-8 mb-4">
+			The data is processed according to the code present on <a class="underline" href="https://github.com/rt-evil-inc/gira-mais-web">https://github.com/rt-evil-inc/gira-mais-web</a>. Data will not be tied to your identity and it is used to generate public usage dashboards and to report errors to the developers.
+		</div>
+	</div>
+
+	<Footer class="bg-muted/40" />
+</div>
